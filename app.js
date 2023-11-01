3df0a73e2caac11ac2c3b4dd797cb5aa28785959
countdaylove <110994423+countdaylove@users.noreply.github.com>
2023-11-01 19:04:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
-const yourDate = new Date("2022-07-12T01:00:00"),
-    music = ['anhtheday', 'vuilamnha', 'xinmaruocemve', 'seetinh', 'matmoc'];
+var yourDate = new Date("2022-07-12 00:00:00");
+var music = ['Nothin', 'Tinhyeunguquen', 'thichemhoinhiu', 'motngaychangnang'];
+// const Nothin = "Nothin On Me",
+//     Tinhyeunguquen ="Tình Yêu Ngủ Quên",
+//     thichemhoinhiu="Thích Em Hơi Nhìu",
+//     motngaychangnang="Một Ngày Chẳng Nắng";
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var rootTime = document.querySelector("time");
+    var month1 = 0, year1 = 0;
+    var currentDate = new Date();
+    var yearDiff = currentDate.getFullYear() - yourDate.getFullYear();
+    var timeDiff = currentDate - yourDate;
+    var dayDiff = currentDate.getDate() - yourDate.getDate();
+    var monthDiff = currentDate.getMonth() - yourDate.getMonth();
+    dayDiff = dayDiff % 30;
+    document.querySelector("anni").textContent = formatDate(yourDate);;
+    if (dayDiff < 0) {
+        monthDiff--;
+        var lastDayOfLastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+        dayDiff += lastDayOfLastMonth.getDate();
+    }
+    document.querySelector("date").textContent = Math.abs(dayDiff) + " DAYS";
+    document.querySelector("year").textContent = yearDiff + " YEAR";
+    if (monthDiff < 0) {
+        yearDiff--;
+        monthDiff += 12;
+    }
+    document.querySelector("month").textContent = monthDiff + " MONTH";
 
-    document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
+    function formatDate(date) {
+        var day = date.getDate();
+        var month = date.getMonth() + 1; // Tháng tính từ 0
+        var year = date.getFullYear();
 
-    document.querySelector("date").textContent = Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) + " DAYS";
-
+        return `${(day > 9 ? day : "0" + day)}-${(month > 9 ? month : "0" + month)}-${year}`;
+    }
     function olock() {
-        var today = new Date(),
-            hrs = (Math.floor(Math.floor((today - yourDate) / 1000) / 60 / 60)) % 24,
-            min = (Math.floor(Math.floor((today - yourDate) / 1000) / 60)) % 60,
-            sec = Math.floor((today - yourDate) / 1000) % 60;
-        rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
+        var today = new Date();
+        var timeDiff = today - yourDate;
+        var hrs = Math.floor(timeDiff / (1000 * 60 * 60)) % 24;
+        var min = Math.floor(timeDiff / (1000 * 60)) % 60;
+        var sec = Math.floor(timeDiff / 1000) % 60;
+        rootTime.textContent = `${(hrs > 9) ? hrs : "0" + hrs}:${(min > 9) ? min : "0" + min}:${(sec > 9) ? sec : "0" + sec}`;
     }
     olock();
-    var timer = setInterval(function() { olock() }, 1000);
-    document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+    var timer = setInterval(function () { olock() }, 1000);
+    document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random() * music.length)]}.mp3`);
 
     document.getElementsByTagName("body")[0].insertAdjacentHTML(
         "beforeend",
         "<div id='mask'></div>"
     );
+
 }, false);
